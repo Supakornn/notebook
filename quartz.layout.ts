d1ae1b1bd4816993ab6bd12c8afb8ea5bf03ccd2
Supakornn <78318848+Supakornn@users.noreply.github.com>
2025-10-05 14:52:16 +0700

    vault backup: 2025-10-05 14:52:16

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -98,6 +98,12 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/supakornn",
+      X: "https://x.com/supak0rnn",
+      Bluesky: "https://bsky.app/profile/supakorn.info",
+      LinkedIn: "https://www.linkedin.com/in/supakornieamgomol/",
+      Facebook: "https://www.facebook.com/spkngap",
+      Instagram: "https://www.instagram.com/supakornigm/",
+      Resume: "https://resume.supakorn.info/",
     },
   }),
 }
